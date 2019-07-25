842e1cfa5f0f98a5db97ccc53322bab052616952
adhithyaakella <53162148+adhithyaakella@users.noreply.github.com>
2019-07-25 12:30:08 +0530

    Update MyRunss.java

--- src/test/java/runnerzz/MyRunss.java
+++ src/test/java/runnerzz/MyRunss.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="MyFeat", glue="steDefsss")
+@CucumberOptions(features="MyFeat", glue="steDefsss", plugin={"pretty","json:target/cucumber.json","html:target/cucumber-htmlreport.html"})
 public class MyRunss {
 
 }
